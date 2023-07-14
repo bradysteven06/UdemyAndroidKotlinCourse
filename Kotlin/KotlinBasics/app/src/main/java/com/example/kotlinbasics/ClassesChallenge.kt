0ac5ec18b3a454e76bb6e9878bb50b144d411fde
Steven Brady <bradys@my.lanecc.edu>
2023-07-14 04:06:20 -0700

    finished Classes Challenge

--- Kotlin/KotlinBasics/app/src/main/java/com/example/kotlinbasics/ClassesChallenge.kt
+++ Kotlin/KotlinBasics/app/src/main/java/com/example/kotlinbasics/ClassesChallenge.kt
@@ -1,13 +1,21 @@
 package com.example.kotlinbasics
 
 fun main() {
-
+    var galaxyS20 = MobilePhoneChallenge("Android","Samsung", "Galaxy S20")
+    galaxyS20.chargeBattery(30)
 }
 
 class MobilePhoneChallenge(osName: String, brand: String, model: String){
-
+    var battery: Int = 15
 
     init {
         println("The phone $model from $brand uses $osName as its Operating System")
+    }
+
+    fun chargeBattery(charged: Int) {
+        println("Starting battery level: $battery")
+        println("Battery was charged by: $charged")
+        println("Current batter charge: ${battery + charged}")
+        battery += charged
     }
 }
